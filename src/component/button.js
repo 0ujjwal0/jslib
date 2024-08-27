aedee4a3f97c03c10c485ce4109cb0ba3a7778a0
ujjwal <ujjwal.12100544@lpu.in>
2024-08-28 00:57:48 +0530

    swaps using ref

--- src/component/button.js
+++ src/component/button.js
@@ -1,16 +1,45 @@
-import React from 'react'
-import { useAudio } from '../context/audioContext';
+import React, { useEffect, useRef, useState } from "react";
+import { useAudio } from "../context/audioContext";
+import { createSwapy } from "swapy";
+import SwapyItems from "./swapyItems";
 
 const Home = () => {
-    const {ClickSound}=useAudio();
-    const handleClick=()=>{
-        ClickSound();
+  const { ClickSound } = useAudio();
+  const containerRef = useRef(null);
+  const swapyRef = useRef(null);
+  const [on,setOn]=useState(true);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      swapyRef.current = createSwapy(containerRef.current, {
+        animation: "dynamic",
+      });
     }
+  }, []);
+
+  const handleClick = () => {
+    ClickSound();
+    setOn(true)
+    if (swapyRef.current) {
+      
+      swapyRef.current.enable(on);
+      setOn(false);
+    }
+  };
+
   return (
-    <div className=' bg-gray-300 h-[100vh] flex items-center justify-end '>
-      <button className='m-10 border-2 border-violet-300 bg-white rounded-md px-2 text-violet-600 hover:text-white font-mono hover:bg-violet-800 hover:shadow-md hover:shadow-violet-950' onClick={handleClick}>click me</button>
+    <div className="bg-gray-300 h-[100vh] flex items-center justify-center">
+      <button
+        className="m-10 border-2 border-violet-300 bg-white rounded-md px-2 text-violet-600 hover:text-white font-mono hover:bg-violet-800 hover:shadow-md hover:shadow-violet-950"
+        onClick={handleClick}
+      >
+        enable swap
+      </button>
+      <div ref={containerRef}>
+        <SwapyItems swapper={handleClick} />
+      </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
---
+++ src/component/swapyItems.js
@@ -0,0 +1,79 @@
+import React from "react";
+const SwapyItems = () => {
+  return (
+    <div className="parent">
+      <div
+        className="w-auto h-auto"
+        data-swapy-slot="doo"
+      >
+        <div
+          className="flex flex-col rounded-2xl w-80 bg-[#ffffff] shadow-xl"
+          data-swapy-item="d"
+        >
+          <figure className="flex justify-center items-center rounded-2xl"></figure>
+          <div className="flex flex-col p-8">
+            <div className="text-3xl font-bold uppercase text-center text-[#374151] pb-6">
+              swappable 1
+            </div>
+            <div className=" text-base  text-center text-[#374151]">
+              try swapping with other cards
+            </div>
+            <div className="flex justify-end pt-6">
+              <button className="bg-[#7e22ce] text-[#ffffff]  font-bold text-sm  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform">
+                tryyy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="w-auto h-auto"
+        data-swapy-slot="eoo"
+      >
+        <div
+          className="flex flex-col rounded-2xl w-80 bg-[#ffffff] shadow-xl"
+          data-swapy-item="e"
+        >
+          <div className="flex flex-col p-8">
+            <div className="text-3xl font-bold uppercase text-center text-[#374151] pb-6">
+              swappable 2
+            </div>
+            <div className=" text-base  text-center text-[#374151]">
+              try swapping with other cards
+            </div>
+            <div className="flex justify-end pt-6">
+              <button className="bg-[#7e22ce] text-[#ffffff]  font-bold text-sm  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform">
+                tryyy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="w-auto h-auto"
+        data-swapy-slot="aoo"
+      >
+        <div
+          className="flex flex-col rounded-2xl w-80 bg-[#ffffff] shadow-xl"
+          data-swapy-item="ae"
+        >
+          <div className="flex flex-col p-8">
+            <div className="text-3xl font-bold uppercase text-center text-[#374151] pb-6">
+              swappable 3
+            </div>
+            <div className=" text-base  text-center text-[#374151]">
+              try swapping with other cards
+            </div>
+            <div className="flex justify-end pt-6">
+              <button className="bg-[#7e22ce] text-[#ffffff]  font-bold text-sm  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform">
+                tryyy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SwapyItems;
